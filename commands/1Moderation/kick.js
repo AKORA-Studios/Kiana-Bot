03cd2f32451a2aa3cf6aa4172911be1dd1a3c966
Kiara <frida.shivaa@gmail.com>
2021-04-24 12:54:16 +0200

    Update kick.js

--- commands/1Moderation/kick.js
+++ commands/1Moderation/kick.js
@@ -23,26 +23,10 @@ module.exports = {
      */
     async execute(msg, args) {
         let emb = rawEmb(msg)
-        let reason = 'Kicked by ' + msg.author.tag;
+        let member = msg.mentions.members.first() || msg.guild.members.resolve(args[0])
 
-        let M,
-            user
-
-        if (msg.mentions.members.first()) {
-            user = msg.mentions.members.first()
-            M = user
-
-        } else if (!isNaN(args[0])) {
-            user = msg.guild.members.resolve(args[0])
-            if (!user) {
-                user = await msg.client.users.fetch(args[0]);
-                user.user = user;
-            }
-            M = user
-
-        } else return msg.channel.send(emb.setColor(colors.error).setDescription("Please enter a user, user-id or role")).catch()
-
-        if (M.bannable == false) return msg.channel.send(emb.setColor(colors.error).setDescription("**This user is not kickable**")).catch()
+        if (!member) return msg.channel.send(emb.setColor(colors.error).setDescription("Please enter a user or user-id ")).catch()
+        if (!member.kickable) return msg.channel.send(emb.setColor(colors.error).setDescription("**This user is not kickable**")).catch()
 
         await user.kick().catch()
         msg.channel.send(emb.setDescription(`**Kicked user**\n ${M.user.tag}`)).catch();
